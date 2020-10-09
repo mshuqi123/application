2b7b579b5b8124cdf7cb2e3358fbd79976208d16
mshuqi123 <1170460010@qq.com>
2020-10-09 20:26:33 +0800

    增加wsgi

---
+++ wsgi.py
@@ -0,0 +1,19 @@
+# import os
+# from dotenv import load_dotenv
+#
+# dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+# if os.path.exists(dotenv_path):
+#     load_dotenv(dotenv_path)
+#
+# from bluelog import create_app  # noqa
+#
+# app = create_app('production')
+
+from app import create_app
+from settings import Settings
+from init import init_tools
+
+init_tools()
+
+
+app = create_app(Settings)
